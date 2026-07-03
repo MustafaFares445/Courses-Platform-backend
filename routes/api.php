3813a639b879aa2240b10314c3e222344aebc82b
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-03 18:50:19 +0300

    Add auth and admin user routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\Admin\AdminUserController;
+use App\Http\Controllers\API\Admin\UserController;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('auth')->group(function (): void {
+    Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 });
+
+Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
+
+Route::middleware(['auth:sanctum', 'admin'])
+    ->prefix('admin')
+    ->group(function (): void {
+        Route::get('users', [UserController::class, 'index']);
+        Route::get('users/{user}', [UserController::class, 'show']);
+        Route::patch('users/{user}/status', [UserController::class, 'updateStatus']);
+
+        Route::middleware('super-admin')->group(function (): void {
+            Route::get('admin-users', [AdminUserController::class, 'index']);
+            Route::post('admin-users', [AdminUserController::class, 'store']);
+            Route::get('admin-users/{adminUser}', [AdminUserController::class, 'show']);
+            Route::patch('admin-users/{adminUser}', [AdminUserController::class, 'update']);
+        });
+    });
